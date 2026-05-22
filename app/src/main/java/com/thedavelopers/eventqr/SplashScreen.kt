a89e86a578f8c9fd55117ea2e99d4e94108dab86
Archinix <kou.tanka0945@gmail.com>
2026-05-22 23:52:03 +0800

    Initial Commit

---
+++ app/src/main/java/com/thedavelopers/eventqr/SplashScreen.kt
@@ -0,0 +1,11 @@
+package com.thedavelopers.eventqr
+
+import android.app.Activity
+import android.os.Bundle
+
+class SplashScreen : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
